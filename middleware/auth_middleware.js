12f2a794e9a3cc05c9e90f1562220e68640c48d4
AhmedMoEssam1512 <11410120236012@stud.cu.edu.eg>
2025-08-27 20:24:54 +0300

    general protect added

--- middleware/auth_middleware.js
+++ middleware/auth_middleware.js
@@ -66,8 +66,45 @@ const studentProtect = async (req, res, next) => {
 };
 
 
+
+const protect = asyncWrapper(async (req, res, next) => {
+   let token;
+
+  // 1. Support for "Authorization: Bearer <token>"
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  // 2. Support for ?token=abc in query (for EventSource)
+  if (!token && req.query.token) {
+    token = req.query.token;
+  }
+
+  if (!token) {
+    return next(new AppError('Not authorized, no token', 401));
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (decoded.type === 'admin') {
+      req.admin = decoded;
+    } else if (decoded.type === 'student') {
+      req.student = decoded;
+    } else {
+      return next(new AppError('Not authorized, invalid token type', 401));
+    }
+    next();
+  } catch (error) {
+    return next(new AppError('Not authorized, token failed', 401));
+  }
+
+});
+
+
+
 module.exports = { 
   adminProtect,
-  studentProtect
+  studentProtect,
+  protect
  };
 
